c0bda2362f4bb2def5517e0fb62d95ee0cf49f73
pbraer <73088589+pbraer@users.noreply.github.com>
2021-05-01 22:34:20 +0700

    correct

--- main.py
+++ main.py
@@ -6,6 +6,114 @@ turtle.pensize(2)
 turtle.Screen().bgcolor('black')
 
 
+# squares function
+def square_draw(a):
+    if a < 5:
+        return
+    turtle.up()
+    turtle.right(15)
+    turtle.forward(a/8)
+    turtle.down()
+    for i in range(4):
+        turtle.forward(a)
+        turtle.right(90)
+    turtle.up()
+    return square(a*0.9)
+
+
+def square(a):
+    turtle.up()
+    turtle.goto(-50, 50)
+    turtle.down()
+    square_draw(a)
+
+
+# Koch's curve
+
+
+def koch_curve_draw(ln):
+    if ln > 6:
+        ln //= 3
+        koch_curve_draw(ln)
+        turtle.left(60)
+        koch_curve_draw(ln)
+        turtle.right(120)
+        koch_curve_draw(ln)
+        turtle.left(60)
+        koch_curve_draw(ln)
+    else:
+        turtle.fd(ln)
+        turtle.left(60)
+        turtle.fd(ln)
+        turtle.right(120)
+        turtle.fd(ln)
+        turtle.left(60)
+        turtle.fd(ln)
+
+
+def koch_curve(ln):
+    turtle.up()
+    turtle.goto(-ln * 1.2, 0)
+    turtle.down()
+    koch_curve_draw(ln)
+
+
+# Koch's snowflake function
+
+
+def koch_step(ln):
+    if ln > 6:
+        ln //= 3
+        koch_step(ln)
+        turtle.left(60)
+        koch_step(ln)
+        turtle.right(120)
+        koch_step(ln)
+        turtle.left(60)
+        koch_step(ln)
+    else:
+        turtle.fd(ln)
+        turtle.left(60)
+        turtle.fd(ln)
+        turtle.right(120)
+        turtle.fd(ln)
+        turtle.left(60)
+        turtle.fd(ln)
+
+
+def koch(ln):
+    turtle.home()
+    turtle.up()
+    turtle.goto(-200, 120)
+    turtle.down()
+    koch_step(ln)
+    turtle.right(120)
+    koch_step(ln)
+    turtle.right(120)
+    koch_step(ln)
+
+
+# Minkovsky's curve
+
+# ?
+def mink(m):
+    if m < 5:
+        return
+    else:
+        turtle.forward(m)
+        turtle.left(90)
+        turtle.forward(m)
+        turtle.right(90)
+        turtle.forward(m)
+        turtle.right(90)
+        turtle.forward(2 * m)
+        turtle.left(90)
+        turtle.forward(m)
+        turtle.left(90)
+        turtle.forward(m)
+        turtle.right(90)
+        turtle.forward(m)
+
 
 # dragon curve function
 
@@ -38,7 +146,7 @@ def dragon(steps, size):
     turtle.forward(steps)
 
 
-# Levi curve function
+# Levi's curve function
 
 def levi_fun(steps, size):
     if size == 0:
@@ -57,6 +165,5 @@ def levi(steps, size):
     turtle.down()
     levi_fun(steps, size)
 
-#dragon(12, 5)
-#levi(10, 8)
+
 turtle.done()
